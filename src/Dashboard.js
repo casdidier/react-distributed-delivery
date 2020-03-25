08446db0a56c377d0fda49cc9b489efe5d372a24
casdidier <cheung.didier@gmail.com>
2020-03-25 22:53:31 +0100

    setup project + POC Graph

---
+++ src/Dashboard.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react'
+import StreamChart from './StreamChart';
+import { managerData, yearLabels, nationalAverageData } from "./mockData";
+import classes from "./Dashboard.module.css";
+export default class Dashboard extends Component {
+
+    state = {
+    data   : managerData,
+    average: nationalAverageData,
+    labels : yearLabels
+    }
+    render() {
+        const { data, average, labels } = this.state;
+        return (
+            <div className={classes.container}>
+            <header>
+                {/* <img src={chartIcon} alt="bar chart icon" /> */}
+                <h1>Stream Dashboard</h1>
+            </header>
+                <StreamChart
+                    data    = {data}
+                    average = {average}
+                    labels  = {labels} />
+            </div>
+        )
+    }
+}
