727c197d21df22d4d662027ad3c6bd99bba0d713
casdidier <cheung.didier@gmail.com>
2020-03-29 21:33:45 +0200

    changed tooltip format

--- src/components/Charts/StackedAreaChart.js
+++ src/components/Charts/StackedAreaChart.js
@@ -1,19 +1,43 @@
 import React from 'react';
 import {
-    AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip
+    AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer
 } from 'recharts';
+
+const renderTooltipContent = (o) => {
+	const { payload, label } = o;
+  // console.log(JSON.stringify(payload, null, 4));
+  const humanDate = payload.humanDate;
+  
+  return (
+  	<div className="customized-tooltip-content">
+    	{/* <p className="date">{`${label} (${humanDate})`}</p> */}
+      <p className="total">{`${label} ${humanDate}`}</p>
+      <ul className="list">
+      	{
+        	payload.map((entry, index) => (
+          	<li key={`item-${index}`} style={{color: entry.color}}>
+            	{`${entry.name}: ${entry.value}`}
+            </li>
+          ))
+        }
+      </ul>
+    </div>
+  );
+};
 
 const StackedAreaChart = ({data}) => {
   	return (
-    	<AreaChart width={600} height={400} data={data}
-            margin={{top: 10, right: 30, left: 0, bottom: 0}}>
-        <CartesianGrid strokeDasharray="3 3"/>
-        <XAxis dataKey="chartDate"/>
-        <YAxis/>
-        <Tooltip/>
-        <Area type='monotone' dataKey='p2p' stackId="1" stroke='#8884d8' fill='#8884d8' />
-        <Area type='monotone' dataKey='cdn' stackId="1" stroke='#82ca9d' fill='#82ca9d' />
-      </AreaChart>
+      <ResponsiveContainer width='100%' height={300} background-color={"#000000"}> 
+        <AreaChart data={data}
+              margin={{top: 10, right: 30, left: 0, bottom: 0}}>
+          <CartesianGrid strokeDasharray="3 3"/>
+          <XAxis dataKey="chartDate"/>
+          <YAxis/>
+          <Tooltip content={renderTooltipContent}/>
+          <Area type='monotone' dataKey='p2p' stackId="1" stroke='#8884d8' fill='#8884d8' />
+          <Area type='monotone' dataKey='cdn' stackId="1" stroke='#82ca9d' fill='#82ca9d' />
+        </AreaChart>
+      </ResponsiveContainer>
     );
   }
 
